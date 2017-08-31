7fc33379f00ce38afe2a6e456c5b54980ba79ffb
Håvard Hellem <haavardhellem@gmail.com>
2017-08-31 16:20:27 +0200

    Added functionallity for cycling through images with buttons and arrow keys on keyboard

---
+++ static/js.js
@@ -0,0 +1,44 @@
+var gImages = ["images/31efa5e14ba4472113b861c77a779147.jpg",
+    "images/d48488fdfa843c615c88607674f30223.jpg",
+    "images/download.jpg",
+    "images/pannekakebilde.jpg",
+    "images/download1.jpg",
+    "images/img_8146.jpg",
+    "images/large.jpg",
+    "images/lg.png"],
+    gImageIndex = Math.floor(Math.random() * (gImages.length - 1));
+
+set_image(gImageIndex);
+
+function prev_image() {
+    if ((gImageIndex - 1) >= 0) {
+        set_image(--gImageIndex);
+    } else {
+        gImageIndex = gImages.length - 1;
+        set_image(gImageIndex);
+    }
+}
+
+function next_image() {
+    if ((gImageIndex + 1) < gImages.length) {
+        set_image(++gImageIndex);
+    } else {
+        gImageIndex = 0;
+        set_image(gImageIndex);
+    }
+}
+
+function set_image(pImageIndex) {
+    document.getElementById("image").src = gImages[pImageIndex];
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '37') {
+        prev_image(); // left arrow.
+    } else if (e.keyCode == '39') {
+        next_image(); // right arrow.
+    }
+}
